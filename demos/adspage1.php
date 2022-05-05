ec29ddba9de51d74142282a6c67cd8a09fc1e4e4
wvtv <98718240+wvtv@users.noreply.github.com>
2022-05-04 21:34:42 -0400

    Add files via upload

---
+++ demos/adspage1.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>viajeros unidos venbra</title>
+<meta name="generator" content="WYSIWYG Web Builder 17 Trial Version - https://www.wysiwygwebbuilder.com">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="font-awesome.min.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Barlow+Condensed:500,300&display=swap" rel="stylesheet">
+<link href="365tv1.22.css" rel="stylesheet">
+<link href="adspage1.css" rel="stylesheet">
+<script src="jquery-3.6.0.min.js"></script>
+<script src="wwb17.min.js"></script>
+</head>
+<body 
+
+>
+<a href="https://www.wysiwygwebbuilder.com" target="_blank"><img src="images/builtwithwwb17.png" alt="WYSIWYG Web Builder" style="position:absolute;left:240px;top:0px;margin: 0;border-width:0;z-index:250" width="16" height="16"></a>
+<div id="wb_LayoutGrid1">
+<div id="LayoutGrid1">
+<div class="row">
+<div class="col-1">
+<div id="wb_Image1" style="display:inline-block;width:100%;height:auto;z-index:0;">
+<img src="images/Instagram-Stories-viaja-a-brasi2382.png" id="Image1" alt="" width="238" height="359">
+</div>
+<div id="wb_Breadcrumb1" style="display:inline-block;width:238px;height:31px;z-index:1;vertical-align:top;">
+<ul id="Breadcrumb1">
+<li><a href="javascript:popupwnd('https://linktr.ee/viajerosunidosvb','no','no','no','no','no','no','','','','')" target="_self" title="No se preocupe, no se cerrará su TV"><i class="fa fa-chevron-down">&nbsp;</i>Hacer una Consulta </a></li>
+<li><a href="javascript:history.back()"><i class="fa fa-chevron-up">&nbsp;</i>Volver a Canales</a></li>
+</ul>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
